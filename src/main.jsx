ac602fd2ac492440b93b162638ffd4fcc46c8099
AlOdinson <mail.alik.kamaletdinov@gmail.com>
2026-09-08 00:57:41 +0800

    feat: load dock style accessories

--- src/main.jsx
+++ src/main.jsx
@@ -10,5 +10,7 @@ import './ipad-system-color-palette.css';
 import './ipad-system-color-palette-scale.css';
 import './floating-drawing-controls-enhancer.js';
 import './ipad-system-color-palette.js';
+import './dock-style-accessories.css';
+import './dock-style-accessories.js';
 
 createRoot(document.getElementById('root')).render(<App />);
